9f0074efaa9e645ceaae1cb48f88ad676b934ad5
Zuhair AlSader <zalsader@hotmail.com>
2025-08-22 00:52:55 -0400

    tidy up
    Signed-off-by: Zuhair AlSader <zalsader@hotmail.com>

--- linux/switch_input.sh
+++ linux/switch_input.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-
 CAHNNEL=$1
 MONITOR=$2
-                                                                                                               A        B    C    D       E       F    G
 solaar config "M720 Triathlon" change-host $CAHNNEL &
-#                                                                                                                 A       B       C   D       E      F    G
 solaar config "K850" change-host $CAHNNEL &
-# Switch the monitors
 ddcutil setvcp 60 0x1${MONITOR} -d 1 &
 ddcutil setvcp 60 0x1${MONITOR} -d 2 &
-
 exit 0
-
-
-
