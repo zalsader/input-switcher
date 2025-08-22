abe31035961754b581593291194a63d1ba7cace6
Zuhair AlSader <zalsader@hotmail.com>
2025-08-22 00:54:44 -0400

    ok maybe this
    Signed-off-by: Zuhair AlSader <zalsader@hotmail.com>

--- linux/switch_input.sh
+++ linux/switch_input.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CAHNNEL=$1
 MONITOR=$2
-solaar config "M720 Triathlon" change-host $CAHNNEL &
-solaar config "K850" change-host $CAHNNEL &
 ddcutil setvcp 60 0x1${MONITOR} -d 1 &
 ddcutil setvcp 60 0x1${MONITOR} -d 2 &
+solaar config "M720 Triathlon" change-host $CAHNNEL
+solaar config "K850" change-host $CAHNNEL
 exit 0
