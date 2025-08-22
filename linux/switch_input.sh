ec166f0e272023fc104c912428c1048167234f81
Zuhair AlSader <zalsader@hotmail.com>
2025-08-21 23:44:53 -0400

    add monitors
    Signed-off-by: Zuhair AlSader <zalsader@hotmail.com>

--- linux/switch_input.sh
+++ linux/switch_input.sh
@@ -4,12 +4,12 @@ CAHNNEL=$1
 MONITOR=$2
 
 # KYB_CH = channel on unifying receiver for the destination keyboard. Typically maps to the 1 minus the key # you press to switch keyboards
-KYB_CH=0x0$CAHNNEL
+KYB_CH=0x0${CAHNNEL}
 # KYB_ID = ID of the keyboard (typically this maps to the order of keyboard devices you see in the unifying software app in Advanced settings)
 KYB_ID=0x03
 
 # MSE_CH = channel on unifying receiver for the destination mouse. Typically maps 1 minus the channel button you press on the mouse to switch
-MSE_CH=0x0$CAHNNEL
+MSE_CH=0x0${CAHNNEL}
 # MSE_ID = ID of the mouse (typically this maps to the order of mouse devices you see in the unifying software app in Advanced settings)
 MSE_ID=0x02
 
@@ -24,6 +24,10 @@ hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 
 # Switch MX Mouse to other device
 #                                                                                                                 A       B       C   D       E      F    G
 hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${MSE_ID},0x0a,0x1b,${MSE_CH},0x00,0x00
+
+# Switch the monitors
+ddcutil setvcp 60 0x1${MONITOR} -d 1
+ddcutil setvcp 60 0x1${MONITOR} -d 2
 
 exit 0
 
